d5885e79f2a543956fddcda8f5661821aae581d2
Robert <37461212+ButterflyBloodlust@users.noreply.github.com>
2021-02-19 21:11:36 +0100

    Reorganized layout. Added input processing on page load in case of refresh. Added single html app file with minified css and js inside.

--- src/js/init.js
+++ src/js/init.js
@@ -1,10 +1,10 @@
-document.getElementById('yt-vid-urls').oninput = function() {
+function processInput(input) {
     const ytIDLength = 11
     const regex = /(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w-_]+)/gmi
     let videoIDs = []
     let m;
     let vidIdsErrorNum = 0;
-    while ((m = regex.exec(this.value)) !== null) {
+    while ((m = regex.exec(input)) !== null) {
         // This is necessary to avoid infinite loops with zero-width matches
         if (m.index === regex.lastIndex) {
             regex.lastIndex++;
@@ -21,19 +21,22 @@ document.getElementById('yt-vid-urls').oninput = function() {
         });
     }
     if (videoIDs.length > 0) {
-        var playlistUrl = "https://www.youtube.com/watch_videos?video_ids=" + videoIDs.join(',');
+        var playlistUrl = 'https://www.youtube.com/watch_videos?video_ids=' + videoIDs.join(',');
         document.getElementById('playlist-url').href = playlistUrl;
         document.getElementById('playlist-url').text = playlistUrl;
     } else {
-        document.getElementById('playlist-url').text = "waiting for proper input...";
-        document.getElementById('playlist-url').removeAttribute("href");
+        document.getElementById('playlist-url').text = 'waiting for proper input...';
+        document.getElementById('playlist-url').removeAttribute('href');
     }
 
     if (vidIdsErrorNum > 0) {
-        document.getElementById('vid-id-error').style.visibility = "visible"
-        document.getElementById('vid-id-error-num').innerText = vidIdsErrorNum
+        document.getElementById('vid-id-error').style.display = 'unset';
+        document.getElementById('vid-id-error-num').innerText = vidIdsErrorNum;
     } else {
-        document.getElementById('vid-id-error').style.visibility = "hidden"
+        document.getElementById('vid-id-error').style.display = 'none';
     }
 };
 
+document.getElementById('yt-vid-urls').oninput = (event) => processInput(event.target.value);
+
+processInput(document.getElementsByTagName('textarea')[0].value);
